de52d6092f241aaafb2fd21d3f6200ba69c69a84
Michael Skyba <michaelskyba2@gmail.com>
2026-08-09 19:36:42 -0400

    filter out legacy code

---
+++ archive/cathedral/__init__.py
@@ -0,0 +1,10 @@
+"""Cathedral - A memory system"""
+
+from .config import (
+    CathedralSettings,
+    DEFAULT_COMPRESSION_RATIO,
+    COMPRESSION_PROFILES,
+)
+
+__version__ = "0.1.0"
+__all__ = ["CathedralSettings", "DEFAULT_COMPRESSION_RATIO", "COMPRESSION_PROFILES"]
